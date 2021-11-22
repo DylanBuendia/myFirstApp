70a8cc288af5e106241ea49278702e852f31d311
Marco Costantino <marco.costantino89@gmail.com>
2021-11-22 12:36:59 +0100

    services on

--- src/app/project/project-container/project-container.component.ts
+++ src/app/project/project-container/project-container.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Project } from '@app/models/Project';
+import { LogService } from '@app/shared/log.service';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'ngprj-project-container',
@@ -10,58 +12,26 @@ import { Project } from '@app/models/Project';
 export class ProjectContainerComponent implements OnInit {
   selectedProject! : Project;
 
-  projects : Project[]= [
-    {
-      id: 1,
-      code: 'afis1m4mmt',
-      name: 'cazz n sacc',
-      description: 'k ruttur i pall',
-      start: new Date(2019,1,30),
-      end:new Date(2021,5,30),
-      done: true,
-      priority: 'medium',
-      task:[],
-    },
-    {
-      id: 2,
-      code: '4fis1ktmurt',
-      name: 'cazz n vo fò',
-      description: 'k ruttur i chigghiun',
-      start: new Date(2020,1,15),
-      end:new Date(2021,6,30),
-      done: true,
-      priority: 'high',
-      task:[],
-    },
-    {
-      id: 3,
-      code: '4fis1k1t4stramurt',
-      name: 'u cazz k ti frica',
-      start: new Date(2020,6,30),
-      done: false,
-      priority: 'medium',
-      task:[],
-    },
-  ];
+  projects : Project[]= [];
 
-  constructor() {
-    // setTimeout(()=> this.projects=[] ,10*1000)
-   }
+  constructor(private projectService : ProjectService) {}
 
   ngOnInit(): void {
+    this.projects = this.projectService.getAll();
   }
 
   selectProject(project:Project){
-    this.selectedProject = project;
+    this.selectedProject = this.projectService.get(project.id);
   }
 
   submitProjectForm(project : Project){
-    this.projects.push({
-      ...project,
-      id : this.projects.length,
-      code : Math.random().toString(36).replace('0.','').substring(2,9),
-      done : false,
-      task : [],
-    });
+    this.projectService.add(project);
+    // this.projects.push({
+    //   ...project,
+    //   id : this.projects.length,
+    //   code : Math.random().toString(36).replace('0.','').substring(2,9),
+    //   done : false,
+    //   task : [],
+    // });
   }
 }
