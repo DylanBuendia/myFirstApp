e8e6cdd1ae54bbbd4aedd243b9c2c270a53fbf89
Marco Costantino <84793848+DylanBuendia@users.noreply.github.com>
2022-04-07 23:02:18 +0200

    fix project service

--- src/app/modules/project/services/project.service.ts
+++ src/app/modules/project/services/project.service.ts
@@ -11,40 +11,6 @@ import { tap, find, retry, catchError } from 'rxjs/operators';
 })
 export class ProjectService {
 
-  private projects : Project[] = [
-    {
-      id: 1,
-      code: 'afis1m4mmt',
-      name: 'cazz n sacc',
-      description: 'k ruttur i pall',
-      start: new Date(2019,1,30),
-      end:new Date(2021,5,30),
-      done: true,
-      priority: 'medium',
-      task:[],
-    },
-    {
-      id: 2,
-      code: '4fis1ktmurt',
-      name: 'cazz n vo fò',
-      description: 'k ruttur i chigghiun',
-      start: new Date(2020,1,15),
-      end:new Date(2021,6,30),
-      done: true,
-      priority: 'high',
-      task:[],
-    },
-    {
-      id: 3,
-      code: '4fis1k1t4stramurt',
-      name: 'u cazz k ti frica',
-      start: new Date(2020,6,30),
-      done: false,
-      priority: 'medium',
-      task:[],
-    }
-  ]
-
   constructor(private httpClient : HttpClient, private logservice : LogService) { }
 
   private projectSubject = new BehaviorSubject<Project[]>(this.projects);
